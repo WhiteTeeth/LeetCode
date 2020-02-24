2bde1464d109435c34d8a7c67cb8fc8b0bf61860
engleliu <engleliu@tencent.com>
2020-02-25 01:48:28 +0800

    Initial Commit

---
+++ LeetCode/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  LeetCode
+//
+//  Created by baiya on 2020/2/25.
+//  Copyright © 2020 baiya. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
