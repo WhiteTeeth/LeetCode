2df94737482699ba0d3b622e9ee16f11c27cf961
engleliu <engleliu@tencent.com>
2020-03-05 21:45:30 +0800

    add No.62. 不同路径

---
+++ LeetCode/leetcode/No.62.uniquePaths.h
@@ -0,0 +1,25 @@
+//
+//  No.62.uniquePaths.h
+//  LeetCode
+//
+//  62. 不同路径
+//  https://leetcode-cn.com/problems/unique-paths/
+//  Created by baiya on 2020/3/5.
+//  Copyright © 2020 baiya. All rights reserved.
+//
+
+#ifndef No_62_uniquePaths_h
+#define No_62_uniquePaths_h
+
+
+int uniquePaths(int m, int n) {
+    vector<vector<int>> dp(m, vector<int>(n , 1));
+    for (int i = 1; i < m; i++) {
+        for (int j = 1; j < n; j++) {
+            dp[i][j] = dp[i-1][j] + dp[i][j-1];
+        }
+    }
+    return dp[m-1][n-1];
+}
+
+#endif /* No_62_uniquePaths_h */
