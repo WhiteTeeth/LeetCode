b73ce4833443184d38356a6b7f90f58967631f69
WhiteTeeth <lhwhiteteeth@gmail.com>
2020-03-14 17:29:09 +0800

    update main file

--- LeetCode/main.mm
+++ LeetCode/main.mm
@@ -7,13 +7,45 @@
 //
 
 #import <Foundation/Foundation.h>
-#import "leetcode/No778_SwimInWater.h"
+#include <iostream>
+//#import "leetcode/No778_SwimInWater.hpp"
+#import "leetcode/No.15.threeSum.h"
+#import "leetcode/No.72.minDistance.h"
+#import "leetcode/No.239.maxSlidingWindow.hpp"
+#import "leetcode/No.84.largestRectangleArea.h"
+#import "leetcode/No.128.longestConsecutive.hpp"
+#import "leetcode/No.32.longestValidParentheses.h"
+#import "leetcode/No.78.subsets.h"
+#import "leetcode/No.416.canPartition.h"
+#import "leetcode/No.64.minPathSum.h"
+#import "leetcode/No.494.findTargetSumWays.h"
+#import "leetcode/No.647.countSubstrings.h"
+#import "leetcode/No.300.lengthOfLIS.h"
+#import "leetcode/No.139.wordBreak.h"
 
-int main(int argc, const char * argv[]) {
+int main(int argc, const char * argr[]) {
     @autoreleasepool {
         // insert code here...
         NSLog(@"Hello, World!");
         
+//        vector<vector<int>> c = {{0,1,2,3,4},{24,23,22,21,5},{12,13,14,15,16},{11,17,18,19,20},{10,9,8,7,6}};
+//        vector<vector<int>> c = {{0,2}, {1,3}};
+//        int result = swimInWater(c);
+//        vector<int> input = {13,11,-3,-1,6,-11,-11,-12,1,-11,-10,12,3,-11,0,9,3,-13,-10,-2,6,14,9,-4,-9,-3,-9,-15,-10,4,-7,10,1,-3,-5,14,1,-9,-13,-12,-4,-8,7,6,-4,-8,0,6,-14,-3,-11,0,-4,13,3,2,-13,9,2,14,-1,10,10,7,14,12,6,0,-12,0,-5,-9,7,-13,12,10,-13,-3,1,10,9,5,-5,12,-5,13,-1,-11,0,-14,-11,6,3,14,-2,2,11,11,10,6,-4,12,13,10,7,10,5,1};
+//        vector<int> input = {0, 0, 0, 0};
+//        vector<vector<int>> re = threeSum(input);
+//        NSLog(@"result = %d", re.size());
+        
+//        testMinDistance();
+//        testNo239();
+//        testLargestRectangleArea();
+//        testLongestValidParentheses();
+//        testMinPathSum();
+//        testFindTargetSumWays();
+//        testCountSubstrings();
+//        testLengthOfLIS();
+        testWordBreak();
+        NSLog(@"test end");
     }
     return 0;
 }
