3369f7eefd560a4606d498b59dec91dd8f704391
baiya <lhwhiteteeth@gmail.com>
2020-03-18 02:12:36 +0800

    add No.215. 数组中的第K个最大元素

---
+++ LeetCode/leetcode/No.215.findKthLargest.h
@@ -0,0 +1,25 @@
+//
+//  No.215.findKthLargest.h
+//  LeetCode
+//
+//  Created by baiya on 2020/3/18.
+//  Copyright © 2020 baiya. All rights reserved.
+//
+
+#ifndef No_215_findKthLargest_h
+#define No_215_findKthLargest_h
+
+#include <vector>
+#include <queue>
+
+// TODO 手写算法
+int findKthLargest(vector<int>& nums, int k) {
+    priority_queue<int,vector<int>,greater<int>> q;
+    for(auto it:nums){
+        q.push(it);
+        if(q.size()>k) q.pop();
+    }
+    return q.top();
+}
+
+#endif /* No_215_findKthLargest_h */
