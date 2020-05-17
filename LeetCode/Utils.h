df1be594a4a8e32bb6c6eb48b053b83ade0e4344
baiya <lhwhiteteeth@gmail.com>
2020-05-17 16:39:38 +0800

    add utils

--- LeetCode/Utils.h
+++ LeetCode/Utils.h
@@ -10,6 +10,7 @@
 #define Utils_h
 
 #include <vector>
+#include <stack>
 #include <string>
 #include <sstream>
 
@@ -30,6 +31,25 @@ void printVector(vector<int> nums) {
     std::ostringstream oss;
     std::copy(nums.begin(), nums.end(), std::ostream_iterator<int>(oss, ","));
     std::cout << oss.str() << std::endl;
+}
+
+TreeNode *convert2Tree(vector<int> nums) {
+    TreeNode *node = new TreeNode(nums[0]);
+    stack<TreeNode*> nodes;
+    nodes.push(node);
+    for (int i = 1; i < nums.size();) {
+        TreeNode *curNode = nodes.top();
+        nodes.pop();
+        int left = nums[i++];
+        curNode->left = new TreeNode(left);
+        nodes.push(curNode->left);
+        if (i < nums.size()) {
+            int right = nums[i++];
+            curNode->right = new TreeNode(right);
+            nodes.push(curNode->right);
+        }
+    }
+    return node;
 }
 
 #endif /* Utils_h */
